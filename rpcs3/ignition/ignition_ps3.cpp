ff4323011b66bf998777931fbd6843715b38aafb
samto6 <123419830+samto6@users.noreply.github.com>
2026-08-27 00:49:21 -0400

    ignition module: include video_source.h for make_video_source
    The make_video_source callback returns unique_ptr<video_source>, whose
    destructor needs the complete type; headless_application includes this
    header for the same reason.

--- rpcs3/ignition/ignition_ps3.cpp
+++ rpcs3/ignition/ignition_ps3.cpp
@@ -14,6 +14,7 @@
 #include "Emu/Cell/Modules/cellOskDialog.h"
 #include "Emu/Cell/Modules/cellSaveData.h"
 #include "Emu/Cell/Modules/sceNpTrophy.h"
+#include "util/video_source.h"
 
 #include <cstdio>
 #include <deque>
